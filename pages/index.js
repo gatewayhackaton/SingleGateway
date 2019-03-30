54ba4bf02c706df7da7d792bdadadf76ce8c1788
djedou <djedouarnaud.aman@gmail.com>
2019-03-30 12:46:39 +0100

    react initialize

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Hackathon extends Component{
+
+    render() {
+        return(
+            <div>
+                Coucou
+            </div>
+        )
+    }
+}
+
+export default Hackathon;
